cd96db9e675d97ae03aa274558ad1d877af64551
Kazu <nokaha1222@gmail.com>
2022-07-08 16:29:26 +0900

    User name added

--- FirstHT.java
+++ FirstHT.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class FirstHT {
     public static void main(String[] args) {
+        System.out.println("Who are you?");
+        Scanner sc = new Scanner(System.in);
+        String input_text = sc.next();
+        System.out.println("Hello, " + input_text + "!");
         int cnt = 0, cnt2 = 0;
         System.out.println("Tossing a coin...");
         String[] twoside = { "Heads", "Tails" };
